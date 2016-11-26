da04e2460e2f011ae6673b3c1ec66af45a2995bf
Pavan Karasala <karasalapavankumar@gmail.com>
2016-11-26 23:55:04 +0000

    Active

---
+++ src/main/java/Sample.java
@@ -0,0 +1,11 @@
+import com.sun.deploy.uitoolkit.ui.LoggerConsole;
+
+/**
+ * Created by Pavan_Karasala on 11/26/2016.
+ */
+public class Sample {
+
+    public static void main(String args[]){
+        System.out.println("Welcome to Spring + GIT");
+    }
+}
---
+++ src/test/java/Test.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Pavan_Karasala on 11/26/2016.
+ */
+
+public class Test {
+    public void TestMethod(){
+
+    }
+}
